62902aecfdfdafad7165fc3c7d92213d2e4a7471
imahnama <muannama25@gmail.com>
2020-09-29 11:41:38 +0300

    Implement the App component

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ButtonPanel from './ButtonPanel';
+import Display from './Display';
+
+export default function App() {
+  return (
+    <div>
+    <Display />
+    <ButtonPanel />
+    </div>
+  );
+}
