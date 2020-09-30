dcbf9562eeaecb2c50fd29f5d5909bfe8333f486
imahnama <muannama25@gmail.com>
2020-09-30 12:53:25 +0300

    fix linter errors

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,3 +1,5 @@
+/* eslint-disable  react/default-props-match-prop-types */
+/* eslint-disable  react/require-default-props */
 import React from 'react';
 import PropTypes from 'prop-types';
 
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -5,33 +5,33 @@ export default function ButtonPanel() {
   return (
     <div className="groups">
       <div className="Group1 d-flex justify-content-around">
-        <Button name="AC" color="#EFEFEF"/>
-        <Button name="+/-" color="#EFEFEF"/>
-        <Button name="%" color="#EFEFEF"/>
+        <Button name="AC" color="#EFEFEF" />
+        <Button name="+/-" color="#EFEFEF" />
+        <Button name="%" color="#EFEFEF" />
         <Button name="÷" />
       </div>
       <div className="Group2 d-flex justify-content-around">
-        <Button name="7" color="#EFEFEF"/>
-        <Button name="8" color="#EFEFEF"/>
-        <Button name="9" color="#EFEFEF"/>
+        <Button name="7" color="#EFEFEF" />
+        <Button name="8" color="#EFEFEF" />
+        <Button name="9" color="#EFEFEF" />
         <Button name="X" />
       </div>
       <div className="Group3 d-flex justify-content-around">
-        <Button name="4" color="#EFEFEF"/>
-        <Button name="5" color="#EFEFEF"/>
-        <Button name="6" color="#EFEFEF"/>
+        <Button name="4" color="#EFEFEF" />
+        <Button name="5" color="#EFEFEF" />
+        <Button name="6" color="#EFEFEF" />
         <Button name="-" />
       </div>
       <div className="Group4 d-flex justify-content-around">
-        <Button name="1" color="#EFEFEF"/>
-        <Button name="2" color="#EFEFEF"/>
-        <Button name="3" color="#EFEFEF"/>
-        <Button name="+"/>
+        <Button name="1" color="#EFEFEF" />
+        <Button name="2" color="#EFEFEF" />
+        <Button name="3" color="#EFEFEF" />
+        <Button name="+" />
       </div>
       <div className="Group5 d-flex">
-        <Button name="0" wide color="#EFEFEF"/>
+        <Button name="0" wide color="#EFEFEF" />
         <Button name="." color="#EFEFEF" />
-        <Button name="="  />
+        <Button name="=" />
       </div>
     </div>
   );
