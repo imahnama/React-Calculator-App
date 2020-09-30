085a585d394173d60098730d27c5f22caca1a0ee
imahnama <muannama25@gmail.com>
2020-09-30 12:48:03 +0300

    Add background color to buttons

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -3,35 +3,35 @@ import Button from './Button';
 
 export default function ButtonPanel() {
   return (
-    <div className="Groups">
+    <div className="groups">
       <div className="Group1 d-flex justify-content-around">
-        <Button name="AC" />
-        <Button name="+/-" />
-        <Button name="%" />
+        <Button name="AC" color="#EFEFEF"/>
+        <Button name="+/-" color="#EFEFEF"/>
+        <Button name="%" color="#EFEFEF"/>
         <Button name="÷" />
       </div>
       <div className="Group2 d-flex justify-content-around">
-        <Button name="7" />
-        <Button name="8" />
-        <Button name="9" />
+        <Button name="7" color="#EFEFEF"/>
+        <Button name="8" color="#EFEFEF"/>
+        <Button name="9" color="#EFEFEF"/>
         <Button name="X" />
       </div>
       <div className="Group3 d-flex justify-content-around">
-        <Button name="4" />
-        <Button name="5" />
-        <Button name="6" />
+        <Button name="4" color="#EFEFEF"/>
+        <Button name="5" color="#EFEFEF"/>
+        <Button name="6" color="#EFEFEF"/>
         <Button name="-" />
       </div>
       <div className="Group4 d-flex justify-content-around">
-        <Button name="1" />
-        <Button name="2" />
-        <Button name="3" />
-        <Button name="+" />
+        <Button name="1" color="#EFEFEF"/>
+        <Button name="2" color="#EFEFEF"/>
+        <Button name="3" color="#EFEFEF"/>
+        <Button name="+"/>
       </div>
-      <div className="Group5 d-flex justify-content-around">
-        <Button name="0" />
-        <Button name="." />
-        <Button name="=" />
+      <div className="Group5 d-flex">
+        <Button name="0" wide color="#EFEFEF"/>
+        <Button name="." color="#EFEFEF" />
+        <Button name="="  />
       </div>
     </div>
   );
